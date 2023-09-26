d1fb5f54eb63974b1ed104d4676ba528cb41549d
priyanshusharman <priyanshuk1900@gmaul.com>
2023-09-26 19:00:06 +0530

    online data storage start

--- db.js
+++ db.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
-const mongoURI = "mongodb://0.0.0.0:27017/legalXpo";
+// const mongoURI = "mongodb://0.0.0.0:27017/legalXpo";
+const mongoURI = "mongodb+srv://aidpriyanshu:GjMK5Y0OE1g9Jj4E@legalxpertise.4fnl0il.mongodb.net/?retryWrites=true&w=majority";
 
 const connectToMongo = () => {
     mongoose.connect(mongoURI)
